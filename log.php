c827b956c249b4f50fcc133c1b98f7308dec2bbd
Angga <42704492+aatupas@users.noreply.github.com>
2023-04-03 00:46:29 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Log Location</title>
+    <title>Log</title>
     <meta charset="utf-8">
     <meta name="author" content="Angga Artupas">
     <meta name="viewport" content="width=device-width, initial-scale=1">
